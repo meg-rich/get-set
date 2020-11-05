94e68f6c52252238c30caaaf351ec48c24fbe1c9
Megan Rich <meganxrich@gmail.com>
2020-11-05 09:01:29 -0800

    Define colors and spacing

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,21 @@
 export interface AppTheme {
     colors: {
-
+        brandBlue: string
+        brandRed: string
+        brandOrange: string
+        brandYellow: string
+        white: string
+        lightGray: string
+        mediumGray: string
+        mediumGray2: string
+        darkGray: string
+    },
+    spacing: {
+        xs: string
+        sm: string
+        default: string
+        md: string
+        lg: string
     }
 }
 
@@ -10,10 +25,17 @@ export const theme: AppTheme = {
         brandRed:  '#ea5455',
         brandOrange: '#f07b3f',
         brandYellow: '#ffd460',
-        white: #ffffff,
-        lighGray: #dadfe1,
-        mediumGray: #bebebe,
-        mediumGray2: #a9a9a9,
-        darkGray: #939393,
+        white: '#ffffff',
+        lightGray: '#dadfe1',
+        mediumGray: '#bebebe',
+        mediumGray2: '#a9a9a9',
+        darkGray: '#939393',
+    },
+    spacing: {
+        xs: '8px',
+        sm: '12px',
+        default: '16px',
+        md: '18px',
+        lg: '24px',
     }
 }
