c20d6d1a38cace70f2e9c54fd3874fb17a23da05
Megan Rich <meganxrich@gmail.com>
2020-11-05 08:53:44 -0800

    Define some color variables

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,6 +6,14 @@ export interface AppTheme {
 
 export const theme: AppTheme = {
     colors: {
-        
+        brandBlue: '#2d4059',
+        brandRed:  '#ea5455',
+        brandOrange: '#f07b3f',
+        brandYellow: '#ffd460',
+        white: #ffffff,
+        lighGray: #dadfe1,
+        mediumGray: #bebebe,
+        mediumGray2: #a9a9a9,
+        darkGray: #939393,
     }
 }
